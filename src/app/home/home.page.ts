e9b81d0d22e2252ff4c02dfd4dcc58488fefe596
Sandeep Suri <sandeep.suri@ryerson.ca>
2019-12-04 21:32:14 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,146 @@
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { formatDate } from '@angular/common';
+import { AlertController } from '@ionic/angular';
+
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Timestamp } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+
+export class HomePage implements OnInit {
+  event = {
+    title: '',
+    description: '',
+    startTime: new Date(),
+    endTime: new Date(),
+    inprogJob: false,
+    compJob: false,
+    allDay: false
+  };
+
+  minDate = new Date().toISOString();
+
+  eventSource = [];
+  viewTitle = '';
+  viewDesc = '';
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
+
+  @ViewChild(CalendarComponent, {static: false}) myCal: CalendarComponent;
+
+  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string, private db: AngularFirestore) { }
+
+  ngOnInit() {
+    this.resetEvent();
+  }
+
+  // Change Month/Week/Date
+  nextBut() {
+    // tslint:disable-next-line: prefer-const
+    let swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
+  }
+
+  backBut() {
+    // tslint:disable-next-line: prefer-const
+    let swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slidePrev();
+  }
+
+  changeView(mode) {
+    this.calendar.mode = mode;
+  }
+
+  addEvent() {
+    let eventCopy = {
+      title: this.event.title,
+      description: this.event.description,
+      startTime: new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay,
+      inprogJob: this.event.inprogJob,
+      compJob: this.event.compJob
+    };
+
+    if (eventCopy.allDay) {
+      const start = eventCopy.startTime;
+      const end = eventCopy.startTime;
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+    }
+
+    this.db.collection(`events`).add(eventCopy);
+    this.eventSource.push(eventCopy);
+
+    this.myCal.loadEvents();
+    this.resetEvent();
+  }
+
+  resetEvent() {
+    this.event = {
+      title: '',
+      description: '',
+      startTime: new Date(),
+      endTime: new Date(),
+      inprogJob: false,
+      compJob: false,
+      allDay: false
+    }
+  };
+
+  async onEventSelected(event) {
+    let start = formatDate(this.event.startTime, 'medium', this.locale);
+    let end = formatDate(this.event.endTime, 'medium', this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: this.event.title,
+      subHeader: this.event.description,
+      message: 'From: ' + start + '<br><br>To: ' + end,
+      buttons: [
+        {
+          text: 'Back',
+          handler: data => {
+            console.log('Back Clicked');
+          }
+        },
+        {
+          text: 'Delete',
+          
+          handler: data => {
+            return this.removeEvent(event);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onDescChanged(description) {
+    this.viewDesc = description;
+  }
+
+  onTimeSelected(ev) {
+    let selected = ev.selectedTime;
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = (selected.toISOString());
+  }
+
+  removeEvent(id) {
+    return this.db.collection(`events`).doc(id).delete();
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCz2_Wijb-tceLoGeQHvGWBnsr8VUvtAV0',
+    authDomain: 'angular-scheduler-3d00c.firebaseapp.com',
+    databaseURL: 'https://angular-scheduler-3d00c.firebaseio.com',
+    projectId: 'angular-scheduler-3d00c',
+    storageBucket: 'angular-scheduler-3d00c.appspot.com',
+    messagingSenderId: '785521189242',
+    appId: '1:785521189242:web:493c81ccc409b0880776c3',
+    measurementId: 'G-3DL572ERXE'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
